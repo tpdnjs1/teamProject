f3ba6b9dfdceef95e840352d8b82ac1d35191b94
tpdnjs1 <errow0607@gmail.com>
2022-11-08 10:32:34 +0900

    11.08
    내용 지움

--- src/main/java/com/example/teamproject/Login.java
+++ src/main/java/com/example/teamproject/Login.java
@@ -1,30 +1,8 @@
 package com.example.teamproject;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class Login {
-    MovePage movePage = new MovePage();
 
-    @FXML
-    private Button loginBtn;
-    @FXML
-    private Label signUp;
-    @FXML
-    private TextField id;
-    @FXML
-    private TextField pw;
 
-    @FXML
-    private void login(){
-        movePage.changeScene("main", loginBtn);
-    }
-
-    @FXML
-    private void moveSignUp(){
-        movePage.changeSceneLabel("SignUp", signUp);
-    }
 
 }
--- src/main/java/com/example/teamproject/SignUp.java
+++ src/main/java/com/example/teamproject/SignUp.java
@@ -1,32 +1,6 @@
 package com.example.teamproject;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class SignUp {
-    MovePage movePage = new MovePage();
 
-    @FXML
-    private Button signUpBtn;
-    @FXML
-    private Label login;
-
-    @FXML
-    private TextField id;
-    @FXML
-    private TextField pw;
-    @FXML
-    private TextField name;
-
-    @FXML
-    private void signUp(){
-        movePage.changeScene("main", signUpBtn);
-    }
-
-    @FXML
-    private void moveLogin(){
-        movePage.changeSceneLabel("Login", login);
-    }
 }

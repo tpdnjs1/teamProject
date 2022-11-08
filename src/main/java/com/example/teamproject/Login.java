13d482d2ee736414435a465fe6c03585658ae389
tpdnjs1 <errow0607@gmail.com>
2022-11-08 10:37:42 +0900

    11.08
    이게 뭐임

--- src/main/java/com/example/teamproject/Login.java
+++ src/main/java/com/example/teamproject/Login.java
@@ -1,8 +1,18 @@
 package com.example.teamproject;
 
+<<<<<<< HEAD
 
 public class Login {
 
 
 
+=======
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Login {
+    @FXML
+    private Button loginBtn;
+    
+>>>>>>> parent of 331b740 (로그인, 회원가입)
 }
--- src/main/java/com/example/teamproject/SignUp.java
+++ src/main/java/com/example/teamproject/SignUp.java
@@ -1,6 +1,10 @@
 package com.example.teamproject;
 
+<<<<<<< HEAD
 
 public class SignUp {
 
+=======
+public class SignUp {
+>>>>>>> parent of 331b740 (로그인, 회원가입)
 }

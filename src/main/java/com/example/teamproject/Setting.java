998eb332ac9c45163ef805b52cf8a4b078460c1e
Kimdoeun06 <gdoeun546@gmail.com>
2022-11-22 14:13:46 +0900

    도은이가 엄청난 일을 해냄ㅋ

--- src/main/java/com/example/teamproject/Setting.java
+++ src/main/java/com/example/teamproject/Setting.java
@@ -1,5 +1,25 @@
 package com.example.teamproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Setting {
+    MovePage movePage = new MovePage();
+
+    @FXML
+    private Button main;
+    @FXML
+    private Button question;
+    @FXML
+    private Button setting;
+
+    @FXML
+    private void moveMain(){movePage.changeScene("Main", main);}
+    @FXML
+    private void moveQuestion(){movePage.changeScene("Questions", question);}
+    @FXML
+    private void moveSetting(){movePage.changeScene("Setting", setting);}
+
 
 }
+

6dd198d41b1804c16573db1164c009f3353d7269
tpdnjs1 <errow0607@gmail.com>
2022-11-22 14:40:18 +0900

    11.22
    캘린더 버튼 연결 중

--- src/main/java/com/example/teamproject/Calendar.java
+++ src/main/java/com/example/teamproject/Calendar.java
@@ -1,4 +1,93 @@
 package com.example.teamproject;
 
 public class Calendar {
+    //윤년 계산
+    private static boolean isLeapYear(int year) {
+        boolean leap = false;
+
+        if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            leap = true;
+        }
+        return leap;
+    }
+
+    private static int getDate(int year, int month) {
+        int tmp = 0;
+
+        if(isLeapYear(year)) {
+
+            switch(month) {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    tmp = 31;
+                    break;
+                case 4: case 6: case 9: case 11:
+                    tmp = 30;
+                    break;
+                case 2:
+                    tmp = 29;
+                    break;
+            }
+        }else {
+            if(month == 1 || month == 3 || month == 5 || month == 7 ||month == 8 || month == 10 || month == 12) {
+                tmp = 31;
+            } else if(month == 4 || month == 6 || month == 9 || month == 11) {
+                tmp = 30;
+            }else if (month == 2) {
+                tmp = 28;
+            }
+        }
+        return tmp;
+    }
+
+    //해당 월의 첫번쨰 요일 계산
+    private static int getDayOfWeek(int year, int month) {
+        int dayOfWeek = 0;
+        int sum = 0;
+
+        // 1.1.1 ~ year-1.12.31
+        for(int i = 1; i < year; i++) {
+            for(int j = 1; j <= 12; j++) {
+                sum += getDate(i, j);
+            }
+        }
+
+        //year.1.1 ~ year.month-1.마지막(31,30.29.28)
+        for(int k = 1; k < month; k++) {
+            sum += getDate(year, k);
+        }
+
+        //year.month.1일
+        sum += 1;
+
+        //요일
+        dayOfWeek = sum % 7;
+
+        return dayOfWeek;
+    }
+
+    public static void prn(int year, int month) {
+        //달력 윗부분 출력
+        System.out.printf("\t\t%d년 %d월\n", year, month);
+        System.out.printf("일\t월\t화\t수\t목\t금\t토\n");
+
+        //시작 요일
+        int start = Calendar.getDayOfWeek(year, month);
+
+        //마지막 날짜
+        int last = getDate(year, month);
+
+        //시작 요일만큼 빈공간
+        for(int i = 1; i <= start; i++) {
+            System.out.print("\t");
+        }
+
+        //달력출력
+        for(int i = 1; i <= last; i++) {
+            System.out.printf("%d\t", i);
+            start++;
+            if(start%7 == 0) {
+                System.out.println();
+            }
+        }
+    }
 }
--- src/main/java/com/example/teamproject/Main.java
+++ src/main/java/com/example/teamproject/Main.java
@@ -2,38 +2,134 @@ package com.example.teamproject;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class Main {
+public class Main implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        date = new ToggleGroup();
+        LocalDate now = LocalDate.now();
+
+    }
+
+
     MovePage movePage = new MovePage();
-
     @FXML
     private Button add;
     @FXML
     private Button main;
     @FXML
     private Button question;
+
     @FXML
     private Button setting;
 
     @FXML
-    private void popUpPage(){
+    private void popUpPage() {
         movePage.popUp("DiaryAdd", add);
     }
-    @FXML
-    private void moveMain(){movePage.changeScene("Main", main);}
-    @FXML
-    private void moveQuestion(){movePage.changeScene("Questions", question);}
-    @FXML
-    private void moveSetting(){movePage.changeScene("Setting", setting);}
 
+    @FXML
+    private void moveMain() {
+        movePage.changeScene("Main", main);
+    }
+
+    @FXML
+    private void moveQuestion() {
+        movePage.changeScene("Questions", question);
+    }
+
+    @FXML
+    private void moveSetting() {
+        movePage.changeScene("Setting", setting);
+    }
+
+    @FXML
+    private ToggleGroup date;
+    @FXML
+    private ToggleButton B1;
+    @FXML
+    private ToggleButton B2;
+    @FXML
+    private ToggleButton B3;
+    @FXML
+    private ToggleButton B4;
+    @FXML
+    private ToggleButton B5;
+    @FXML
+    private ToggleButton B6;
+    @FXML
+    private ToggleButton B7;
+    @FXML
+    private ToggleButton B8;
+    @FXML
+    private ToggleButton B9;
+    @FXML
+    private ToggleButton B10;
+    @FXML
+    private ToggleButton B11;
+    @FXML
+    private ToggleButton B12;
+    @FXML
+    private ToggleButton B13;
+    @FXML
+    private ToggleButton B14;
+    @FXML
+    private ToggleButton B15;
+    @FXML
+    private ToggleButton B16;
+    @FXML
+    private ToggleButton B17;
+    @FXML
+    private ToggleButton B18;
+    @FXML
+    private ToggleButton B19;
+    @FXML
+    private ToggleButton B20;
+    @FXML
+    private ToggleButton B21;
+    @FXML
+    private ToggleButton B22;
+    @FXML
+    private ToggleButton B23;
+    @FXML
+    private ToggleButton B24;
+    @FXML
+    private ToggleButton B25;
+    @FXML
+    private ToggleButton B26;
+    @FXML
+    private ToggleButton B27;
+    @FXML
+    private ToggleButton B28;
+    @FXML
+    private ToggleButton B29;
+    @FXML
+    private ToggleButton B30;
+    @FXML
+    private ToggleButton B31;
+    @FXML
+    private ToggleButton B32;
+    @FXML
+    private ToggleButton B33;
+    @FXML
+    private ToggleButton B34;
+    @FXML
+    private ToggleButton B35;
 }

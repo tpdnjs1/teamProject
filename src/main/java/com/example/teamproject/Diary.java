30c6f835b9398d3c2a2a4c903073f0e4eb9f836e
tpdnjs1 <errow0607@gmail.com>
2022-11-26 20:59:11 +0900

    11.26
    오류 수정

--- src/main/java/com/example/teamproject/Diary.java
+++ src/main/java/com/example/teamproject/Diary.java
@@ -1,9 +1,44 @@
 package com.example.teamproject;
 
-import javafx.fxml.FXML;
-
 import java.util.Date;
 
 public class Diary {
+    private String title;
+    private String stText;
+    private Date date;
 
+    public Diary(String title, Date date, String stText){
+        this.title = title;
+        this.stText = stText;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStText() {
+        return stText;
+    }
+
+    public void setStText(String text) {
+        this.stText = text;
+    }
+
+    @Override
+    public String toString() {
+        return "" + date + " : " + title;
+    }
 }
